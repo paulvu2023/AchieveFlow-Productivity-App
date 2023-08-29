c8d3cfdf7e1cbf819b81151ed3a5f05689b0cd75
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-08-29 12:26:50 -0700

    Add styles.css to src

---
+++ src/todo-items.js
@@ -0,0 +1,9 @@
+class todoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checked = false;
+    }
+}
