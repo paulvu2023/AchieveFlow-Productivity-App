dee9b2a4f5d76a47bd55a3946a6bcb54aafa1cb9
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-04 14:41:36 -0700

    Add Task button now opens task form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css";
 import { toDoItem } from "./toDoItems";
 
+let taskForm = document.getElementById("add-task-form");
+let addTaskButton = document.querySelector('.add-task');
+
+addTaskButton.addEventListener('click', openTaskForm);
+
+function openTaskForm() {
+    taskForm.classList.add('open-add-task-form');
+}
+
+function closeTaskForm() {
+    taskForm.classList.remove('open-add-task-form');
+}
