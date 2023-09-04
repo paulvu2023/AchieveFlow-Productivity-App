c80395061449c38d5a47b825fa125c0c3f2d094c
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-04 14:50:40 -0700

    Add darker and unclickable background when add task form opened

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import "./styles.css";
 import { toDoItem } from "./toDoItems";
 
 let taskForm = document.getElementById("add-task-form");
+let overlay = document.getElementById('overlay');
 
 document.querySelector('.add-task').addEventListener('click', openTaskForm);
 document.querySelector('.cancel-task-button').addEventListener('click', closeTaskForm);
 
 function openTaskForm() {
     taskForm.classList.add('open-add-task-form');
+    overlay.style.display = 'block';
 }
 
 function closeTaskForm() {
     taskForm.classList.remove('open-add-task-form');
+    overlay.style.display = 'none';
 }
