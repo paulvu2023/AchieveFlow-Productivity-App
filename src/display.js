9100905fa1ef96a2b0f7565e084a959443d4069f
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-12 12:47:34 -0700

    Optimize openProjectForm function

--- src/display.js
+++ src/display.js
@@ -215,24 +215,22 @@ function addProjectToSidebar(projectName) {
 function openProjectForm() {
     const container = document.querySelector('.project-container');
     const overlay = document.createElement('div');
-
     overlay.classList.add('overlay');
-    overlay.style.display = 'block';
 
     const formHTML = `
-    <div class="project-form">
-        <form class="add-project-form" id="add-project-form">
-            <fieldset>
-                <label for="project-name">Project Name</label>
-                <input type="text" id="project-name" name="project-name" autocomplete="off" maxlength="15" required spellcheck="false">
-                <div>
-                    <button type="submit" class="add-project-button">Add Project</button>
-                    <button type="button" class="cancel-project-button">Cancel</button>
-                </div>
-            </fieldset>
-        </form>
-    </div>  
-    `
+        <div class="project-form">
+            <form class="add-project-form" id="add-project-form">
+                <fieldset>
+                    <label for="project-name">Project Name</label>
+                    <input type="text" id="project-name" name="project-name" autocomplete="off" maxlength="15" required spellcheck="false">
+                    <div>
+                        <button type="submit" class="add-project-button">Add Project</button>
+                        <button type="button" class="cancel-project-button">Cancel</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    `;
 
     container.insertAdjacentHTML('beforeend', formHTML);
     container.appendChild(overlay);
